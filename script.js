5f76b67396217eeb6e3adfb57ef0a692c6bab48e
AYB <ayyubiy67@gmail.com>
2024-07-18 14:06:25 +0100

    hide player input and sown gameboard on start

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.getElementById("start-btn").addEventListener("click", () => {
   const playerName = document.getElementById("player").value;
 
   if (playerName) {
-    document.getElementById("player-input").classList.add("hidden");
-    document.getElementById("gameboard-container").classList.remove("hidden");
+    document.getElementById("player-input").style.display = "none"; // Hide player input and start button
+    document.getElementById("gameboard-container").classList.remove("hidden"); // Show gameboard
   } else {
     alert("Please enter your name.");
   }
