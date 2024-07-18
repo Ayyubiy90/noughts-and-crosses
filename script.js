02b970df0e56e11a792c2c3fe81b50455f45dc0d
AYB <ayyubiy67@gmail.com>
2024-07-18 13:27:24 +0100

    update to single player input and start game logic

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.getElementById("start-btn").addEventListener("click", () => {
-  const player1Name = document.getElementById("player1").value;
-  const player2Name = document.getElementById("player2").value;
+  const playerName = document.getElementById("player").value;
 
-  if (player1Name && player2Name) {
+  if (playerName) {
     document.getElementById("player-input").classList.add("hidden");
     document.getElementById("gameboard-container").classList.remove("hidden");
   } else {
-    alert("Please enter names for both players.");
+    alert("Please enter your name.");
   }
 });
